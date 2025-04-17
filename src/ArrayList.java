736ec442a7066f8430fe1926aee2ebd780fe1c72
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-04-17 15:17:02 +0530

    Added

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,6 +1,10 @@
 public class ArrayList {
     public static void main(String[] args){
-        
+        int a=10;
+        int b=19;
+
+        int result= a+b;
+        System.out.println(result);
     }
 
 }
